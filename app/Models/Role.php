9268386e9f9ac83745c451e8a526baa45e77c791
Ihor Rud <Igorryd7@gmail.com>
2023-06-16 21:54:10 +0300

    Модель Role

---
+++ app/Models/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    const USER = 1;
+    const ADMIN = 2;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name'
+    ];
+}
