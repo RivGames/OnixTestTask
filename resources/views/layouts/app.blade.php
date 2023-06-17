71e6788913d3aa4bac824a77ee53bf3a4564a824
Ihor Rud <Igorryd7@gmail.com>
2023-06-17 12:34:28 +0300

    Зроблено головний лейаут для додатку

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title> {{env('APP_NAME')}} </title>
+</head>
+<body class="h-full">
+
+<div class="min-h-full">
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">@yield('header')</h1>
+        </div>
+    </header>
+
+    <main>
+        @yield('main')
+    </main>
+</div>
+</body>
+</html>
