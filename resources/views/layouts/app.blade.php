b30f71f6b3caa3e7b9b56704505f322cf136d980
Ihor Rud <Igorryd7@gmail.com>
2023-06-20 07:36:38 +0300

    Перехід на слоти

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,10 @@
 <div class="min-h-full">
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">@yield('header')</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900"> {{ $title }} </h1>
         </div>
     </header>
-
-    <main>
-        @yield('main')
-    </main>
+    {{ $slot }}
 </div>
 @livewireScripts
 </body>
